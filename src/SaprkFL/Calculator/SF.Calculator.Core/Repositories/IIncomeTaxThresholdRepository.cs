fefc307a0edbadbfc09811979638329fac60e661
erloon <erloon@wp.pl>
2018-10-12 21:14:46 +0200

    Working on persistance

---
+++ src/SaprkFL/Calculator/SF.Calculator.Core/Repositories/IIncomeTaxThresholdRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SF.Calculator.Core.Model;
+
+namespace SF.Calculator.Core.Repositories
+{
+    public interface IIncomeTaxThresholdRepository
+    {
+        List<IncomeTaxThreshold> Get(TaxationForm taxationForm);
+
+    }
+}
--- src/SaprkFL/Calculator/SF.Calculator.Core/Services/TaxPercentagesService.cs
+++ src/SaprkFL/Calculator/SF.Calculator.Core/Services/TaxPercentagesService.cs
@@ -1,27 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using SF.Calculator.Core.Model;
+using SF.Calculator.Core.Repositories;
 
 namespace SF.Calculator.Core.Services
 {
     public class TaxPercentagesService : ITaxPercentagesService
     {
+        private readonly IIncomeTaxThresholdRepository _taxThresholdRepository;
+
+        public TaxPercentagesService(IIncomeTaxThresholdRepository taxThresholdRepository)
+        {
+            _taxThresholdRepository = taxThresholdRepository ?? throw new ArgumentNullException(nameof(taxThresholdRepository));
+        }
         public IncomeTaxThreshold GetLinearRate()
         {
-            return new IncomeTaxThreshold(TaxationForm.LINEAR, 1, 0, int.MaxValue, 0.19m);
+            return _taxThresholdRepository.Get(TaxationForm.LINEAR).FirstOrDefault();
+            //return new IncomeTaxThreshold(TaxationForm.LINEAR, 1, 0, int.MaxValue, 0.19m);
         }
 
         public List<IncomeTaxThreshold> GetGeneralIncomeTaxThresholds()
         {
-            return new List<IncomeTaxThreshold>()
-            {
-               new IncomeTaxThreshold(TaxationForm.GENERAL,1,0,85528m,0.18m),
-               new IncomeTaxThreshold(TaxationForm.GENERAL,2,85528m,int.MaxValue,0.32m)
-            };
+            return _taxThresholdRepository.Get(TaxationForm.GENERAL);
+            //return new List<IncomeTaxThreshold>()
+            //{
+            //   new IncomeTaxThreshold(TaxationForm.GENERAL,1,0,85528m,0.18m),
+            //   new IncomeTaxThreshold(TaxationForm.GENERAL,2,85528m,int.MaxValue,0.32m)
+            //};
         }
 
         public List<IncomeTaxThreshold> GetFlatRates()
         {
-            throw new System.NotImplementedException();
+            return _taxThresholdRepository.Get(TaxationForm.FLAT);
+            //throw new System.NotImplementedException();
         }
     }
 }
