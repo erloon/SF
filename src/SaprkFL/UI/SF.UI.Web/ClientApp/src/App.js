70e5179bb614f252dc505a643e2d3d8437b2be05
erloon <erloon@wp.pl>
2018-11-17 22:42:14 +0100

    Added navbar

--- src/SaprkFL/UI/SF.UI.Web/ClientApp/src/App.js
+++ src/SaprkFL/UI/SF.UI.Web/ClientApp/src/App.js
@@ -1,14 +1,14 @@
 ﻿import React from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
+import BaseLayout from './components/Layout/BaseLayout';
 import Home from './components/Home';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
 export default () => (
-  <Layout>
+  <BaseLayout>npm
     <Route exact path='/' component={Home} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
+  </BaseLayout>
 );
